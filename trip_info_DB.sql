1cab5563e4cf9e5930c008e36e7a5d8f5ca395d7
ksy990628 <55133538+ksy990628@users.noreply.github.com>
2019-12-17 22:01:19 +0900

    Add files via upload

---
+++ trip_info_DB.sql
@@ -0,0 +1,33 @@
+INSERT INTO trip_info VALUES
+
+('참 조각 박물관', '베트남', '다낭','2, 2 Thang 9, Hai Chau, Da Nang',
+'100년이 넘은 유서 깊은 박물관, 참파 왕국의 화려했던 과거를 엿볼 수 있는 곳, 유네스코 세계 문화유산인 미선에서 발굴된 유물 감상 가능',
+'http://cfd.tourtips.com/@cms_800/2016120916/gjfcva/%EB%8B%A4%EB%82%AD_%EC%B0%B8%EC%A1%B0%EA%B0%81%EB%B0%95%EB%AC%BC%EA%B4%80_TTB(4).JPG'),
+
+('드래곤 브릿지', '베트남', '다낭','Nguyen Van Linh, Phuoc Ninh, Da Nang',
+'다낭의 랜드마크인 용교, 밤이면 더욱 화려하게 빛나는 다리, 주말 저녁 9시에는 10분간 불 쇼를 진행',
+'https://happist.com/wp-content/uploads/2017/08/%ED%95%9C%EA%B0%95-%EC%95%BC%EA%B2%BD-%EC%9A%A9%EB%8B%A4%EB%A6%AC-6766.jpg'),
+
+('하롱베이', '베트남', '하노이','Ha Long, Quang Ninh, Viet Nam','베트남을 대표하는 여행지, 하롱베이
+유네스코 세계 자연 유산에 등재, 아름다운 섬과 자연을 만날 수 있는 곳','https://post-phinf.pstatic.net/MjAxOTAzMjhfMSAg/MDAxNTUzNzczNTUyODM5.3uJ6RPxcm6TpJ-qIBQYJAsKJKf7nrx1Vn5yyB7wtZhEg.TEh1Dt4bSo110tYPH59SnotYZhRXGvy8CokEE7foVU0g.JPEG/image_9362541601553773518669.jpg?type=w1200'),
+
+('호아로 수용소', '베트남', '하노이','1 Hoa Lo, Tran Hung Dao, Hoan Kiem, Ha Noi','프랑스 식민 시기에 지어진 감옥, 베트남 독립투사들이 수감되었던 곳, 베트남 내전 당시, 미군 포로를 수용하기도','http://storage.doopedia.co.kr/upload/_upload/jwupload/image/201507/13/20150713142655970/20150713142655970_thumb_1024.JPG'),
+
+('응옥선 사당', '베트남', '하노이','Dinh Tien Hoang, Hang Trong, Hoan Kiem, HaNoi','호안끼엠 호수에 위치, 베트남 성인을 모시는 사당, 내부에 박재된 대형 거북이','https://www.indochinatravelpackages.com/wp-content/uploads/2015/06/Ngoc-Son-Temple-1.jpg'),
+
+('노트르담 대성당', '베트남', '호치민','Cong xa Paris, Ben Hghe, Quan 1, HCM',
+'호치민을 대표하는 콜로니얼 건축물, 아름다운 붉은색 외관이 돋보이는 성당, 중앙 우체국과 나란히 위치','https://media.triple.guide/triple-cms/c_limit,f_auto,h_2048,w_2048/04af3240-2dce-4aa3-9c3e-ef09ee92edc1.jpeg'),
+
+('꾸찌터널', '베트남', '호치민','Phu Hiep, Phu My Hung, Cu Chi, HCM',
+'베트남 전쟁의 상징적인 장소, 꾸찌 터널','https://www.wonderfulpackage.com/uploads/moxie/413069a1_bd03dafbdbd38c0705042e4fb2bf31ca_n.jpg'),
+
+('여행자 거리', '베트남', '호치민','De Tham, Pham Ngu Lao, Quan 1, HCM',
+'호치민의 모든 여행자들이 모이는 핫한 거리,데탐(De Tham) 거리에서부터 부이 비엔(Bui Vien)거리까지, 주말 저녁(19:00~02:00)에는 차 없는 거리로 더욱 인산인해를 이루는 곳','http://cdn-asia.heykorean.com.s3.ap-southeast-1.amazonaws.com/community/uploads/images/2019/04/1556103112.jpg'),
+
+('빈펄랜드', '베트남', '나트랑','Hon Tre Island, Vinh Nguyen, Nha Trang 650000, Vietnam',
+'베트남 그룹 VIN이 만든 테마파크. 동물원, 아쿠아리움, 워터파크, 놀이기구를 한 번에! 바다 위를 지나가는 케이블카 타는 묘미',
+'http://cfd.tourtips.com/@cms_800/2018120313/gjfod8/%EB%82%98%ED%8A%B8%EB%9E%91_%EB%B9%88%ED%8E%84%EB%9E%9C%EB%93%9C_TTB%2810%29.jpg'),
+
+ ('탑바 포나가르', '베트남', '나트랑','2 Thang 4, Vinh Phuoc, Thanh Pho Nha Trang, Khanh Hoa 650000',
+'가장 오래된 참파 왕국의 사원 유적, 나트랑 여행자들의 필수 방문지, 늦은 오후에 방문할 것을 추천',
+'https://post-phinf.pstatic.net/MjAxOTA2MTlfMjYg/MDAxNTYwOTI0Mzc0NDU0.pJ-uLTECWxyrfQ1QZg75_oSvtPW97nKx3Sqf-DyY54Yg.Or4hY5ublmEEAJSD3gDuktP14BhQDwXcE8mOGTla8LYg.JPEG/3_%ED%8F%AC%EB%82%98%EA%B0%80%EB%A5%B4_%EC%82%AC%EC%9B%90.jpg?type=w1200');
